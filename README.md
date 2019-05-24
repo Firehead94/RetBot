c05125afc4a8f702aef2301be65630449c21f1b5
Justin Scott <justinjonscott@gmail.com>
2019-05-24 17:16:20 -0400

    Role support

--- README.md
+++ README.md
@@ -20,15 +20,24 @@ Permissions: Admin
 2) !log blacklist
 3) !log setchannel
 4) !log setchannel clear
-5) !ban MEMBER PURGE_LENGTH REASON
+5) !ban MEMBER PURGE (LENGTH) (REASON)
 6) !purge NUMBER
+7) !role reaction EMOJI_ID ROLE_ID
+8) !channel mark/unmark EMOJI_ID
+9) !role add/remove LEVEL_NAME ROLE_ID
+10) !role print (LEVEL_NAME)
+
 ```
-1) This adds/removes the channel the command is run in to the ignore list for activity logging.
-2) Alias for !log blacklist add.
-3) This sets the channel that will hold activity logs based on the channel the commands is run in.
-4) This clears and disables activity logging.
-5) This DMs a MEMBER the ban appeal form and then subsequent bans MEMBER for REASON and purges their activity for PURGE_LENGTH days prior. Reason is optional.
-6) Deletes NUMBER previous messages
+ 1) This adds/removes the channel the command is run in to the ignore list for activity logging.
+ 2) Alias for !log blacklist add.
+ 3) This sets the channel that will hold activity logs based on the channel the commands is run in.
+ 4) This clears and disables activity logging.
+ 5) This DMs a MEMBER the ban appeal form and then subsequent bans MEMBER for REASON and purges their activity for PURGE_LENGTH days prior. Reason is optional.
+ 6) Deletes NUMBER previous messages
+ 7) Adds an EMOJI_ID ROLE_ID pairing so that if a message in a channel that is on watch gets that emoji as a reaction, if will promote that person to the given role.
+ 8) Marks/Unmarks a channel to be monitored for reactions using EMOJI_ID
+ 9) Adds/Removes an internal permission level of LEVEL_NAME that is paired to all ROLE_ID's added to it.
+10) Prints all roles under LEVEL_NAME or all roles if LEVEL_NAME is not given.
 
 ### Staff
 
--- src/Discord/Modules/Roles.py
+++ src/Discord/Modules/Roles.py
@@ -1,6 +1,9 @@
 import sys
 
+import typing
 from discord.ext import commands
+
+from src.Discord.Utils import generic_embed
 
 
 class Roles(commands.Cog):
@@ -29,6 +32,58 @@ class Roles(commands.Cog):
                 await ctx.send(content="{} assigned to {}.".format(ctx.guild.get_role(int(role_id)).mention, self.bot.get_emoji(int(emoji_id))))
         except:
             pass
+
+    @roles.command(name="add", pass_context=True)
+    async def add_role(self, ctx, level, role_id):
+        level = level.lower()
+        try:
+            self.bot.config['guilds'][str(ctx.guild.id)]['roles']
+        except:
+            self.bot.config['guilds'][str(ctx.guild.id)]['roles'] = {}
+            self.bot.save()
+        try:
+            if int(role_id) not in self.bot.config['guilds'][str(ctx.guild.id)]['roles'][level]:
+                self.bot.config['guilds'][str(ctx.guild.id)]['roles'][level].append(int(role_id))
+                self.bot.save()
+                await ctx.send(content='Level {} created.\n{} is now listed as level {}.'.format(level, ctx.guild.get_role(int(role_id)), level))
+            else:
+                await ctx.send(content='Role already assigned to that level.')
+        except:
+            self.bot.config['guilds'][str(ctx.guild.id)]['roles'][level] = [int(role_id)]
+            self.bot.save()
+            await ctx.send(content='{} is now listed as level {}.'.format(ctx.guild.get_role(int(role_id)), level))
+
+    @roles.command(name='remove', pass_context=True)
+    async def remove_role(self, ctx, level, role_id):
+        level = level.lower()
+        try:
+            self.bot.config['guilds'][str(ctx.guild.id)]['roles'][level].remove(int[role_id])
+            self.bot.save()
+            await ctx.send(content='{} removed from level {}.'.format(ctx.guild.get_role(int(role_id)), level))
+        except:
+            await ctx.send('Level doesnt exist.')
+
+    @roles.command(name='print', pass_context=True)
+    async def roles_print(self, ctx, level: typing.Optional[str]=None):
+        if level is None:
+            try:
+                content = ''
+                for level, roles in self.bot.config['guilds'][str(ctx.guild.id)]['roles'].items():
+                    content = content + '**{}**\n'.format(level)
+                    for role in roles:
+                        content = content + '{}\n'.format(ctx.guild.get_role(int(role)).mention)
+                await ctx.send(content=None, embed=generic_embed(title='Internal Roles', description=content, author=self.bot.user))
+            except:
+                await ctx.send(content='No roles found')
+        else:
+            try:
+                level = level.lower()
+                content = ''
+                for role in self.bot.config['guilds'][str(ctx.guild.id)]['roles'][level]:
+                    content = content + '{}\n'.format(ctx.guild.get_role(int(role)).mention)
+                await ctx.send(content=None, embed=generic_embed(title='{}'.format(level), description=content, author=self.bot.user))
+            except:
+                await ctx.send(content='{} not found.'.format(level))
 
     @commands.Cog.listener()
     async def on_message(self, message):
